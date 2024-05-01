eb5adb4d5050f8c25a8f01c4ac4629e1ca48a546
Ohm-Tiwari <otiwari24@gmail.com>
2024-05-01 13:05:09 -0400

    change yml file from main to master

---
+++ src/test/java/edu/iu/otiwari/c322finalbackend/repository/OrderRepositoryTest.java
@@ -0,0 +1,12 @@
+package edu.iu.otiwari.c322finalbackend.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderRepositoryTest {
+
+    @Test
+    void findAllByCustomerUserName() {
+    }
+}
